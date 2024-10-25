0fdc452b749e5ffd8dad12cbdb2462121b2280a7
CC張仁杰 <nk.cho@elgana.works>
2024-10-25 17:11:32 +0900

    refactor(src): 优化代码结构并移除未使用的变量
    - 移除了 task-view.tsx 中的未使用状态变量 navOpen 和 setNavOpen
    - 优化了 test.tsx 中的返回结构，移除了不必要的 Fragment

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -4,8 +4,6 @@ import { TestView } from '../sections/test/view';
 
 export default function Page() {
   return (
-    <>
-      <TestView />
-    </>
+    <TestView />
   );
 }
--- src/sections/task/view/task-view.tsx
+++ src/sections/task/view/task-view.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import { _tasks } from 'src/_mock';
 import { DashboardContent } from 'src/layouts/dashboard';
@@ -9,14 +9,10 @@ import { AnalyticsTasks } from '../../overview/analytics-tasks';
 // ----------------------------------------------------------------------
 
 export function TaskView() {
-  const [navOpen, setNavOpen] = useState(false);
 
   return (
     <DashboardContent>
-
-
-          <AnalyticsTasks title="Tasks" list={_tasks} />
-
+      <AnalyticsTasks title="Tasks" list={_tasks} />
     </DashboardContent>
   );
 }
